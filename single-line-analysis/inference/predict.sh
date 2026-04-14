146c008663b22bb24b2959464f3b6aa15d84ae1a
Khao0 <115398809+Khao0@users.noreply.github.com>
2026-04-14 14:30:20 +0700

    Restructure Data folder

--- single-line-analysis/inference/predict.sh
+++ single-line-analysis/inference/predict.sh
@@ -1,4 +1,4 @@
 curl -X POST http://localhost:8000/single-line-predict \
-    -F "retinal_fundus=@data/retinal_fundus.png" \
-    -F "tomogram=@data/tomogram.png" \
+    -F "retinal_fundus=@../data/retinal_fundus.png" \
+    -F "tomogram=@../data/tomogram.png" \
     -o result.json
